387f5e8fe439e25ce8189014dfc1e737454b2a0f
Jennifer Bell & Alexa Zeazas <student@epicodus.com>
2015-08-18 11:47:03 -0700

    Add save method to animal class

--- lib/animal.rb
+++ lib/animal.rb
@@ -1,5 +1,5 @@
 class Animal
-  attr_reader(:name, :gender, :type, :breed, :admit_date)
+  attr_reader(:name, :gender, :type, :breed, :admit_date, :id)
 
   define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
@@ -7,9 +7,31 @@ class Animal
     @type = attributes.fetch(:type)
     @breed = attributes.fetch(:breed)
     @admit_date = attributes.fetch(:admit_date)
+    @id = attributes.fetch(:id)
   end
 
   define_singleton_method(:all) do
-    []
+    returned_animals = DB.exec("SELECT * FROM animals;")
+    animals = []
+    returned_animals.each() do |animal|
+      name = animal.fetch("name")
+      gender = animal.fetch("gender")
+      type = animal.fetch("type")
+      breed = animal.fetch("breed")
+      admit_date = animal.fetch("admit_date")
+      id = animal.fetch("id").to_i()
+      animals.push(Animal.new({:name => name, :gender => gender, :type => type, :breed => breed, :admit_date => admit_date, :id => id}))
+    end
+    animals
   end
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO animals (name, gender, type, breed, admit_date) VALUES ('#{@name}', '#{@gender}', '#{@type}', '#{@breed}', '#{@admit_date}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+
+  define_method(:==) do |another_animal|
+    self.name().==(another_animal.name()).&(self.id().==(another_animal.id()))
+  end
+
 end
--- spec/animal_spec.rb
+++ spec/animal_spec.rb
@@ -3,7 +3,7 @@ require('spec_helper')
 describe(Animal) do
   describe('#name') do
     it('tells you its name') do
-      test_animal = Animal.new({:name => "Mika", :gender => "female", :type => "cat", :breed => "Turkish Angora", :admit_date => 2015-8-01})
+      test_animal = Animal.new({:name => "Mika", :gender => "female", :type => "cat", :breed => "Turkish Angora", :admit_date => "2015-08-01", :id => nil})
       expect(test_animal.name()).to(eq("Mika"))
     end
   end
@@ -16,9 +16,17 @@ describe(Animal) do
 
   describe('#save') do
     it('adds an animal to the array of saved animals') do
-      test_animal = Animal.new({:name => "Mika", :gender => "female", :type => "cat", :breed => "Turkish Angora", :admit_date => 2015-8-01})
+      test_animal = Animal.new({:name => "Mika", :gender => "female", :type => "cat", :breed => "Turkish Angora", :admit_date => "2015-08-01", :id => nil})
       test_animal.save()
       expect(Animal.all()).to(eq([test_animal]))
+    end
+  end
+
+  describe("#==") do
+    it("is the same animal if it has the same description") do
+      test_animal1 = Animal.new({:name => "Mika", :gender => "female", :type => "cat", :breed => "Turkish Angora", :admit_date => "2015-08-01", :id => nil})
+      test_animal2 = Animal.new({:name => "Mika", :gender => "female", :type => "cat", :breed => "Turkish Angora", :admit_date => "2015-08-01", :id => nil})
+      expect(test_animal1).to(eq(test_animal2))
     end
   end
 
