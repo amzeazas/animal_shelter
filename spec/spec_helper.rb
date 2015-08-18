f00824381ff66b42da06f78c09f99255e3fa78f1
Jennifer Bell & Alexa Zeazas <student@epicodus.com>
2015-08-18 10:33:23 -0700

    Add spec_helper.rb file.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,13 @@
+require('rspec')
+require('pg')
+require('animal')
+require('adopter')
+
+DB = PG.connect({:dbname => "shelter_test"})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM animals *;")
+    DB.exec("DELETE FROM adopters *;")
+  end
+end
