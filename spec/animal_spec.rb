461bc434f8315d2eb4d1099fc26531c1d82c9bf2
Jennifer Bell & Alexa Zeazas <student@epicodus.com>
2015-08-18 11:07:55 -0700

    Add spec for save method to Animal class.

--- spec/animal_spec.rb
+++ spec/animal_spec.rb
@@ -14,4 +14,12 @@ describe(Animal) do
     end
   end
 
+  describe('#save') do
+    it('adds an animal to the array of saved animals') do
+      test_animal = Animal.new({:name => "Mika", :gender => "female", :type => "cat", :breed => "Turkish Angora", :admit_date => 2015-8-01})
+      test_animal.save()
+      expect(Animal.all()).to(eq([test_animal]))
+    end
+  end
+
 end
