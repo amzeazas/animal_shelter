12d4a2ebb406261401afabc9d880cc5ac6a48073
Jennifer Bell & Alexa Zeazas <student@epicodus.com>
2015-08-18 14:32:52 -0700

    Add content to index.erb page.

--- spec/animal_shelter_integration_spec.rb
+++ spec/animal_shelter_integration_spec.rb
@@ -7,7 +7,7 @@ set(:show_exceptions, false)
 describe('adding a new animal', {:type => :feature}) do
   it('allows a user to add an animal to the database') do
     visit('/')
-    click_link('Shelter Workers')
+    click_link('Shelter Employees')
     fill_in('name', :with => "Mika"; 'gender', :with => "female"; 'type', :with => "cat"; 'breed', :with => "Turkish Angora"; 'admit_date', :with => '2015-08-01')
     click_button('Add Animal')
     expect(page).to have_content('Success!')
