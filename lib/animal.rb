29a26c170e96e63a379d09d2d63b3ff9d4779a51
Jennifer Bell & Alexa Zeazas <student@epicodus.com>
2015-08-18 10:47:15 -0700

    Add name method to animal class

---
+++ lib/animal.rb
@@ -0,0 +1,11 @@
+class Animal
+  attr_reader(:name, :gender, :type, :breed, :admit_date)
+
+  define_method(:initialize) do |attributes|
+    @name = attributes.fetch(:name)
+    @gender = attributes.fetch(:gender)
+    @type = attributes.fetch(:type)
+    @breed = attributes.fetch(:breed)
+    @admit_date = attributes.fetch(:admit_date)
+  end
+end
--- spec/animal_spec.rb
+++ spec/animal_spec.rb
@@ -3,7 +3,7 @@ require('spec_helper')
 describe(Animal) do
   describe('#name') do
     it('tells you its name') do
-      test_animal = Animal.new({:name => "Mika", :gender => "female", :type => "cat", :breed => "Turkish Angora" :admit_date => 2015-08-01})
+      test_animal = Animal.new({:name => "Mika", :gender => "female", :type => "cat", :breed => "Turkish Angora", :admit_date => 2015-8-01})
       expect(test_animal.name()).to(eq("Mika"))
     end
   end
