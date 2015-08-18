4b7400f22cdbb4e880d85203c5a30ae3b6241917
Jennifer Bell & Alexa Zeazas <student@epicodus.com>
2015-08-18 10:41:55 -0700

    Add spec for name method to Animal class.

---
+++ spec/animal_spec.rb
@@ -0,0 +1,10 @@
+require('spec_helper')
+
+describe(Animal) do
+  describe('#name') do
+    it('tells you its name') do
+      test_animal = Animal.new({:name => "Mika", :gender => "female", :type => "cat", :breed => "Turkish Angora" :admit_date => 2015-08-01})
+      expect(test_animal.name()).to(eq("Mika"))
+    end
+  end
+end
