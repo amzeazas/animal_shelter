15b644592d58aeba6309a3d9f2ac272af55ce96e
Jennifer Bell & Alexa Zeazas <student@epicodus.com>
2015-08-18 14:15:22 -0700

    Add add animal integration spec.

---
+++ spec/animal_shelter_integration_spec.rb
@@ -0,0 +1,15 @@
+require('spec_helper')
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('adding a new animal', {:type => :feature}) do
+  it('allows a user to add an animal to the database') do
+    visit('/')
+    click_link('Shelter Workers')
+    fill_in('name', :with => "Mika"; 'gender', :with => "female"; 'type', :with => "cat"; 'breed', :with => "Turkish Angora"; 'admit_date', :with => '2015-08-01')
+    click_button('Add Animal')
+    expect(page).to have_content('Success!')
+  end
+end
